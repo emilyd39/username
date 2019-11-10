d09fa39919f96ff2042d01a5c59530673a317308
Emily Dong <edong@chapman.edu>
2019-11-09 17:15:19 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -4,7 +4,7 @@
 echo "The only characters that can be used are lowercase letters, digits, and the underscore character"
 echo "Enter a valid username: "
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{3,12}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "Sorry this is not a valid username"
 	echo "Enter a valid username: "
